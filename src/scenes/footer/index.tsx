a95887d02e42dbed5aed11e1d1bd3f362290fceb
Dawitlior <dawitlior777@gmail.com>
2023-04-12 20:59:31 +0300

    footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -14,13 +14,13 @@ const Footer = () => {
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">https://github.com/Dawitlior</p>
-          <p className="my-5">https://www.linkedin.com/in/lior-dawit-07464b236/</p>
-          <p>https://lior-dawit.com/</p>
+          <p className="my-5"><a href="https://github.com/Dawitlior">GitHub </a></p>
+          <p className="my-5"><a href="https://www.linkedin.com/in/lior-dawit-07464b236/">LinkdIn</a></p>
+          <a href="https://lior-dawit.com/">Portfolio</a>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">liordawit777@walla.com</p>
+          <p className="my-5"><a href="mailto:liordawit777@walla.com">Email-Me</a></p>
           <p>(054)265-3154</p>
         </div>
       </div>
