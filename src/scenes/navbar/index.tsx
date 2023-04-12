27a51cefa8fcb8104ad746afbf58e5fed880ad0d
Dawitlior <dawitlior777@gmail.com>
2023-04-12 10:50:23 +0300

    nav

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -54,7 +54,6 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
                   />
                 </div>
                 <div className={`${flexBetween} gap-8`}>
-                  <p>Sign In</p>
                   <ActionButton setSelectedPage={setSelectedPage}>
                     Become a Member
                   </ActionButton>
